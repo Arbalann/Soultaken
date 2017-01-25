969e859ae12be41a3779f3eab8cf9f690ed1915e
Michael Brown <mbrown10839@gmail.com>
2017-01-25 14:47:20 -0600

    fixed more collision errors

--- Assets/Scripts/Controller2D.cs
+++ Assets/Scripts/Controller2D.cs
@@ -72,16 +72,24 @@ public class Controller2D : MonoBehaviour
 
     void CheckCollisions(ref Vector2 currentVelocity)
     {
+        if (state.previouslyGrounded)
+        {
+            currentVelocity.y = currentVelocity.x * -state.previousSlope;
+        }
+
+        // Get body border and reduce it by skin size
         Bounds bounds = body.bounds;
         bounds.Expand(SKIN_WIDTH * -2f);
-
-        RaycastHit2D hit;
-
+        
+        // Run collision detection until we don't hit anything, limit to 10 times in case we get stuck in a wall
         for (int i = 0; i < 10; i++)
-        { 
-            hit = Physics2D.BoxCast(bounds.center, bounds.size, 0f, currentVelocity, currentVelocity.magnitude + SKIN_WIDTH, TERRAIN_LAYER);
+        {
+            RaycastHit2D hit = Physics2D.BoxCast(bounds.center, bounds.size, 0f, currentVelocity, currentVelocity.magnitude, TERRAIN_LAYER);
+
+            // Stop if we didn't hit anything
             if (hit.collider == null) return;
 
+            // Calculate some numbers we will use later
             Vector2 distanceToHit = hit.centroid - (Vector2)bounds.center;
             Vector2 distanceAfterHit = currentVelocity - distanceToHit;
             float slopeOfCollider = hit.normal.x / hit.normal.y;
@@ -89,34 +97,85 @@ public class Controller2D : MonoBehaviour
             // Hit floor or ceiling
             if (Mathf.Abs(slopeOfCollider) <= MAX_CLIMBABLE_SLOPE)
             {
+                // If we hit both the ground and ceiling in the same frame
+                if ((state.grounded && hit.normal.y < 0) || (state.ceilingCollision && hit.normal.y > 0))
+                {
+                    // Limit horizontal movement so we don't go through the ceiling or floor
+                    currentVelocity.x = distanceToHit.x;
+                    currentVelocity.x += SKIN_WIDTH * Mathf.Sign(hit.normal.x);
+                    distanceAfterHit = currentVelocity - distanceToHit;
+                }
+
+                // Limit the movement to how far away the collider is
                 currentVelocity.y = distanceToHit.y;
+                // Calculate how far up and down we need to move to stay on a slope
                 currentVelocity.y += distanceAfterHit.x * -slopeOfCollider;
                 currentVelocity.y += SKIN_WIDTH * Mathf.Sign(hit.normal.y);
-
+                
+                // Hit ground
                 if (hit.normal.y > 0)
                 {
+                    // Reset overall velocity to cancel out gravity
                     velocity.y = 0;
+
                     state.grounded = true;
                     state.groundSlope = slopeOfCollider;
                 }
-                else if (velocity.y > 0)
+                // Hit ceiling
+                else 
                 {
-                    velocity.y = 0;
+                    state.ceilingCollision = true;
+                    state.groundSlope = slopeOfCollider;
+
+                    // Reset overall velocity so we don't cling to the ceiling
+                    if (velocity.y > 0)
+                    {
+                        velocity.y = 0;
+                    }
                 }
             }
 
             // Hit wall
             else
             {
+                // If grounded
                 if (state.grounded)
                 {
+                    // Move back up/down slope the distance we had to stop early
                     currentVelocity.y -= distanceAfterHit.x * -state.groundSlope;
+                    distanceAfterHit = currentVelocity - distanceToHit;
+                }
+
+                // Find out what we collided with
+                if (hit.normal.x > 0)
+                {
+                    state.leftCollision = true;
+                }
+                else
+                {
+                    state.rightCollision = true;
+                }
+
+                // If we hit both a left and right wall in the same frame
+                if (state.leftCollision && state.rightCollision)
+                {
+                    // Limit vertical movement so we don't go through walls
+                    currentVelocity.y = distanceToHit.y;
+                    currentVelocity.y += SKIN_WIDTH * Mathf.Sign(hit.normal.y);
+                    distanceAfterHit = currentVelocity - distanceToHit;
+
+                    // Landed in a pit "\/"
+                    if (velocity.y < 0)
+                    {
+                        state.grounded = true;
+                    }
                 }
 
                 currentVelocity.x = distanceToHit.x;
                 currentVelocity.x += distanceAfterHit.y / -slopeOfCollider;
                 currentVelocity.x += SKIN_WIDTH * Mathf.Sign(hit.normal.x);
                 velocity.x = 0;
+
             }
         }
     }
@@ -124,13 +183,25 @@ public class Controller2D : MonoBehaviour
     struct CollisionState
     {
         public bool grounded;
+        public bool ceilingCollision;
+        public bool leftCollision;
+        public bool rightCollision;
         public float groundSlope;
         public bool jumping;
+        public bool previouslyGrounded;
+        public float previousSlope;
 
         public void Reset()
         {
+            previouslyGrounded = grounded;
+            previousSlope = groundSlope;
+
             grounded = false;
+            ceilingCollision = false;
+            leftCollision = false;
+            rightCollision = false;
             groundSlope = 0;
+
         }
     }
 }
